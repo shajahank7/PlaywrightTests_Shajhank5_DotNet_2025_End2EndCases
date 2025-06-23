0c3fbe3a6d5868294b45a9beab47066d5ffbbdcd
aadhya4suresh <aadhya4suresh@gmail.com>
2025-06-23 20:45:30 +0530

    updated yml file on 23 Jul 2025

--- obj/Debug/net9.0/PlaywrightTests.AssemblyInfo.cs
+++ obj/Debug/net9.0/PlaywrightTests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PlaywrightTests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d4f3ba340b4b4e28f76c6937c713fe377c625305")]
 [assembly: System.Reflection.AssemblyProductAttribute("PlaywrightTests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PlaywrightTests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
